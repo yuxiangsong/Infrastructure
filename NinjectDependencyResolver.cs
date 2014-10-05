80586a848cc9692082058df5dd7b7e3453fb172f
yuxiangsong <joe.syx@outlook.com>
2014-10-05 15:01:03 +1100

    test
    test

---
+++ NinjectDependencyResolver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Ninject;
+
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Infrastructure.Concrete;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver()
+        {
+            kernel = new StandardKernel();
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
+            kernel.Bind<IAuthProvider>().To<FormsAuthProvider>();
+        }
+    }
+}
